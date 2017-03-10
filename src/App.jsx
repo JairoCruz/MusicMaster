401a2608cb05b6a64417b30eb50de86520a0a5d7
JairoCruz <azaveliz@gmail.com>
2017-03-10 15:48:33 -0600

    24, Preparing a Web Request

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,9 @@ class App extends Component {
 
 	search() {
 		console.log('this.state', this.state);
+		const BASE_URL = 'GET https://api.spotify.com/v1/search?';
+		const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+		console.log('FECH_URL', FETCH_URL);
 	}
 
 	render() {
